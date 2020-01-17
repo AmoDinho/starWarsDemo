ea090e94d6834180faf4cb78ee9945bc98232f5f
amo.moloko <amogelang@tripplo.co>
2020-01-17 17:57:13 +0200

    Trying to add button

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ import {
   View,
   Text,
   StatusBar,
+  Button
 } from 'react-native';
 
 import {
@@ -36,7 +37,7 @@ const App: () => React$Node = () => {
       setPeople(myJSOn)
     })
   }
-  
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
@@ -51,6 +52,7 @@ const App: () => React$Node = () => {
             </View>
           )}
           <View style={styles.body}>
+            <View> </View>
             <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>Step One</Text>
               <Text style={styles.sectionDescription}>
